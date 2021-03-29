e443cbc4a140a3ac2d915eedd4cfce4b15074e46
Joacquim Mukadi <joacquimm@gmail.com>
2021-03-29 18:10:42 +0000

    implemented tail function which returns tail of an array

---
+++ tail.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const errorMessage = `❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`;
+  const successMessage = `✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`;
+  // Below checks for match with assertion. 
+  actual === expected? console.log(successMessage): console.log(errorMessage);
+};
+
+//TAIL function : Returns the "tail" of an array: everything except for the first item (head) of the provided array.
+const tail = (arr) => {
+  let tailArray = [];
+  for(let i = 1; i < arr.length; i++) {
+   tailArray.push(arr[i])
+  }
+
+  console.log(tailArray);
+  return tailArray;
+}
+
+const words = ["Yo Yo", "Lighthouse", "Labs"]
+
+//test case 1: showcases that assertEqual can not  compare array values. 
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+
+//Test case 2(the actual test for tail):  to check that original words array was not modified 
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
